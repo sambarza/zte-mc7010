ef88468527f1d70ff22a016761c8a81e2acb7a05
Samuele Barzaghi <sambarza@gmail.com>
2025-01-06 21:10:39 +0100

    Lock also LTE bands

--- zte_mc7010.py
+++ zte_mc7010.py
@@ -64,6 +64,16 @@ async def get_cmd_process(host, login_token, cmd) -> ZteResponse:
 
 async def set_cmd_process(host, login_token, goformId, data):
 
+    response = await get_cmd_process(
+        host, login_token, cmd="wa_inner_version,cr_version,RD"
+    )
+
+    ad = calculate_ad(
+        response.payload["wa_inner_version"],
+        response.payload["cr_version"],
+        response.payload["RD"],
+    )
+
     async with aiohttp.ClientSession() as session:
 
         async with session.post(
@@ -72,7 +82,7 @@ async def set_cmd_process(host, login_token, goformId, data):
                 "Referer": f"http://{host}/",
                 "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             },
-            data=f"isTest=false&goformId={goformId}&{data}",
+            data=f"isTest=false&goformId={goformId}&{data}&AD={ad}",
             cookies={"stok": login_token},
         ) as response:
 
@@ -110,34 +120,34 @@ async def login(host, password: str) -> str:
 async def get_active_band(host, login_token):
 
     response = await get_cmd_process(
-        host=host, login_token=login_token, cmd="wan_active_band"
+        host=host, login_token=login_token, cmd="wan_active_band%2cnr5g_action_band"
     )
 
-    return response.payload["wan_active_band"]
+    return f"{response.payload['wan_active_band']} / {response.payload['nr5g_action_band']}"
 
 
 async def speed_up(host, login_token):
 
-    response = await get_cmd_process(
-        host, login_token, cmd="wa_inner_version,cr_version,RD"
+    response_4g = await set_cmd_process(
+        host,
+        login_token,
+        "BAND_SELECT",
+        f"is_gw_band=0&gw_band_mask=0&is_lte_band=1&lte_band_mask=0x80001",
     )
 
-    ad = calculate_ad(
-        response.payload["wa_inner_version"],
-        response.payload["cr_version"],
-        response.payload["RD"],
-    )
-
-    response = await set_cmd_process(
+    response_5g = await set_cmd_process(
         host,
         login_token,
         "WAN_PERFORM_NR5G_BAND_LOCK",
-        f"nr5g_band_mask=20%2C1%2C3&AD={ad}",
+        f"nr5g_band_mask=3",
     )
 
-    print(response.payload)
+    payload = {
+        "response_4g": response_4g.payload,
+        "response_5g": response_5g.payload
+    }
 
-    return response.payload
+    return payload
 
 
 # ---------------- M A I N ---------------
@@ -150,11 +160,13 @@ async def main():
     password = config["password"]
 
     login_token = await login(host, password)
-    active_band = await get_active_band(host, login_token)
+    active_band_before = await get_active_band(host, login_token)
     speedup_response = await speed_up(host, login_token)
+    active_band_after = await get_active_band(host, login_token)
 
-    print("Active BAND before speed up:", active_band)
+    print("Active BAND before speed up:", active_band_before)
     print("Speed UP response:", speedup_response)
+    print("Active BAND after speed up:", active_band_after)
 
 
 if __name__ == "__main__":
